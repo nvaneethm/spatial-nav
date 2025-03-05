2fbbdd34a0268597a2d2ca6a5dd2ed17d6a47026
Navaneeth M <navaneeth.murukesh@brightcove.com>
2025-03-05 14:33:21 +0530

    configurable grid

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createGrid } from "./Grid/index.js";
 const grid = document.getElementById("grid");
 
-createGrid(3 * 4, 2, 2, grid);
+const config = {
+  grid,
+  rows: 3,
+  cols: 4,
+  totalItems: 3 * 4,
+  nestedRows: 2,
+  nestedCols: 2,
+};
+
+createGrid(config.totalItems, config.nestedRows, config.nestedCols, grid);

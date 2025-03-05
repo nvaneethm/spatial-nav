87a28d9f60aa70d524547c025c93c4e8fa68b845
Navaneeth M <navaneeth.murukesh@brightcove.com>
2025-03-05 14:53:03 +0530

    navigation handler on key down

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createGrid } from "./Grid/index.js";
 import { updateFocus } from "./FocusManager/index.js";
+import {handleSidebarNavigation, handleTopbarNavigation, handleGridNavigation, handleNestedNavigation } from "./NavigationHandler/index.js"
 
 const sidebarItems = document.querySelectorAll(".sidebar-item");
 const topbarItems = document.querySelectorAll(".topbar-item");
 const grid = document.getElementById("grid");
 
 const config = {
+  sidebarItems,
+  topbarItems,
   grid,
   rows: 3,
   cols: 4,
@@ -23,5 +26,18 @@ const state = {
 };
 
 createGrid(config.totalItems, config.nestedRows, config.nestedCols, grid);
+
+document.addEventListener("keydown", (event) => {
+  if (state.currentState === "sidebar") {
+    handleSidebarNavigation(event, state, config);
+  } else if (state.currentState === "topbar") {
+    handleTopbarNavigation(event, state, config);
+  } else if (state.currentState === "grid") {
+    handleGridNavigation(event, state, config);
+  } else if (state.currentState === "nested") {
+    handleNestedNavigation(event, state, config);
+  }
+  updateFocus(state, { sidebarItems, topbarItems, grid });
+});
 
 updateFocus(state, { sidebarItems, topbarItems, grid });
